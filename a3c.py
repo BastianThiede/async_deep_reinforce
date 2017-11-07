c95d485d4e6bb58f86d7485e0f42b4cb2ecca6af
Bastian Thiede <k4ff33p0tt@gmail.com>
2017-11-07 14:08:40 +0100

    Omitting python 3+ module Path for os.path renaming of variables

--- a3c.py
+++ a3c.py
@@ -1,28 +1,20 @@
 # -*- coding: utf-8 -*-
-import signal
+
 import csv
-import random
+import functools
 import math
 import os
-import time
-import functools
+import signal
 import sys
+import time
 
 import tensorflow as tf
 import threading
-import numpy as np
-from pathlib import Path
 
-
-from game_ac_network import GameACFFNetwork, GameACLSTMNetwork
 from a3c_training_thread import A3CTrainingThread
-from rmsprop_applier import RMSPropApplier
 from config_utils import load_config
-
-
-import yaml
-
-
+from game_ac_network import GameACFFNetwork, GameACLSTMNetwork
+from rmsprop_applier import RMSPropApplier
 
 def log_uniform(lo, hi, rate):
   log_lo = math.log(lo)
@@ -42,12 +34,13 @@ def train_function(parallel_index):
     while True:
         if stop_requested:
             break
-        if global_t > experiment_config['MAX_TIME_STEP']:
+        if global_t > config['MAX_TIME_STEP']:
             break
 
         diff_global_t = training_thread.process(sess, global_t, summary_writer,
                                                 summary_op, score_input)
         global_t += diff_global_t
+
 
 def signal_handler(signal, frame):
   global stop_requested
@@ -55,68 +48,74 @@ def signal_handler(signal, frame):
   stop_requested = True
 
 
+def setup_logfile():
+    fname = 'a3c_training_log_{}.csv'.format(config['GAME'])
+
+    pis = ['PI_{}'.format(i) for i in range(config['ACTION_SIZE'])]
+    fieldnames = pis + ['V', 'Action', 'Score', 'Time',
+                        'Step', 'Episode_reward', 'GameName']
+
+    if os.path.isfile(fname):
+        csvfile = open(fname, 'a')
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    else:
+        csvfile = open(fname, 'w')
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+    return csvfile, writer
+
+
 if len(sys.argv) > 1:
     config_path = sys.argv[1]
 else:
     config_path = None
 
-experiment_config = load_config(config_path)
-print(experiment_config)
+config = load_config(config_path)
 
 device = "/cpu:0"
-if experiment_config['USE_GPU']:
+if config['USE_GPU']:
     device = "/gpu:0"
 
 
 
 
 
-initial_learning_rate = log_uniform(experiment_config['INITIAL_ALPHA_LOW'],
-                                    experiment_config['INITIAL_ALPHA_HIGH'],
-                                    experiment_config['INITIAL_ALPHA_LOG_RATE'])
+initial_learning_rate = log_uniform(config['INITIAL_ALPHA_LOW'],
+                                    config['INITIAL_ALPHA_HIGH'],
+                                    config['INITIAL_ALPHA_LOG_RATE'])
 
 global_t = 0
 
 stop_requested = False
 
-if experiment_config['USE_LSTM']:
-    global_network = GameACLSTMNetwork(experiment_config['ACTION_SIZE'], -1, device)
+if config['USE_LSTM']:
+    global_network = GameACLSTMNetwork(config['ACTION_SIZE'], -1, device)
 else:
-    global_network = GameACFFNetwork(experiment_config['ACTION_SIZE'], -1, device)
+    global_network = GameACFFNetwork(config['ACTION_SIZE'], -1, device)
 
 training_threads = []
 
 learning_rate_input = tf.placeholder("float")
 
 grad_applier = RMSPropApplier(learning_rate=learning_rate_input,
-                              decay=experiment_config['RMSP_ALPHA'],
+                              decay=config['RMSP_ALPHA'],
                               momentum=0.0,
-                              epsilon=experiment_config['RMSP_EPSILON'],
-                              clip_norm=experiment_config['GRAD_NORM_CLIP'],
+                              epsilon=config['RMSP_EPSILON'],
+                              clip_norm=config['GRAD_NORM_CLIP'],
                               device=device)
-fname = 'a3c_training_log_{}.csv'.format(experiment_config['GAME'])
-a3c_log = Path(fname)
-pis = ['PI_{}'.format(i) for i in range(experiment_config['ACTION_SIZE'])]
-fieldnames = pis + ['V','Action','Score','Time',
-                    'Step','Episode_reward','GameName']
 
-if a3c_log.is_file():
-    csvfile = open(fname,'a')
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-else:
-    csvfile = open(fname, 'w')
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+csvfile, writer = setup_logfile()
 
 
-for i in range(experiment_config['PARALLEL_SIZE']):
+for i in range(config['PARALLEL_SIZE']):
     training_thread = A3CTrainingThread(i, global_network,
                                         initial_learning_rate,
                                         learning_rate_input,
                                         grad_applier,
-                                        experiment_config['MAX_TIME_STEP'],
+                                        config['MAX_TIME_STEP'],
                                         device=device,
-                                        config=experiment_config,
+                                        config=config,
                                         logger=(csvfile,writer))
 
     training_threads.append(training_thread)
@@ -134,12 +133,12 @@ tf.summary.scalar("score", score_input)
 
 
 summary_op = tf.summary.merge_all()
-summary_writer = tf.summary.FileWriter(experiment_config['LOG_FILE'], sess.graph)
+summary_writer = tf.summary.FileWriter(config['LOG_FILE'], sess.graph)
 
 
 # init or load checkpoint with saver
 saver = tf.train.Saver()
-checkpoint = tf.train.get_checkpoint_state(experiment_config['CHECKPOINT_DIR'])
+checkpoint = tf.train.get_checkpoint_state(config['CHECKPOINT_DIR'])
 if checkpoint and checkpoint.model_checkpoint_path:
     saver.restore(sess, checkpoint.model_checkpoint_path)
     print("checkpoint loaded:", checkpoint.model_checkpoint_path)
@@ -148,7 +147,7 @@ if checkpoint and checkpoint.model_checkpoint_path:
     global_t = int(tokens[1])
     print(">>> global step set: ", global_t)
     # set wall time
-    wall_t_fname = experiment_config['CHECKPOINT_DIR'] + '/' + 'wall_t.' + str(global_t)
+    wall_t_fname = config['CHECKPOINT_DIR'] + '/' + 'wall_t.' + str(global_t)
     with open(wall_t_fname, 'r') as f:
         wall_t = float(f.read())
 else:
@@ -166,7 +165,7 @@ train_func_partial = functools.partial(train_function,
                                        score_input=score_input)
 
 
-for i in range(experiment_config['PARALLEL_SIZE']):
+for i in range(config['PARALLEL_SIZE']):
     train_threads.append(
         threading.Thread(target=train_function, args=(i,)))
 
@@ -177,29 +176,23 @@ start_time = time.time() - wall_t
 
 for t in train_threads:
     t.start()
-    print('Hey')
 
 print('Press Ctrl+C to stop')
 
-# create_occasional_backup()
-
-
 signal.pause()
-print
-"Nay"
 
 print('Now saving data. Please wait')
 
 for t in train_threads:
     t.join()
 
-if not os.path.exists(experiment_config['CHECKPOINT_DIR']):
-    os.mkdir(experiment_config['CHECKPOINT_DIR'])
+if not os.path.exists(config['CHECKPOINT_DIR']):
+    os.mkdir(config['CHECKPOINT_DIR'])
 
     # write wall time
 wall_t = time.time() - start_time
-wall_t_fname = experiment_config['CHECKPOINT_DIR'] + '/' + 'wall_t.' + str(global_t)
+wall_t_fname = config['CHECKPOINT_DIR'] + '/' + 'wall_t.' + str(global_t)
 with open(wall_t_fname, 'w') as f:
     f.write(str(wall_t))
 
-saver.save(sess, experiment_config['CHECKPOINT_DIR'] + '/' + 'checkpoint', global_step=global_t)
+saver.save(sess, config['CHECKPOINT_DIR'] + '/' + 'checkpoint', global_step=global_t)
